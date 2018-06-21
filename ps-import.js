31f72622d28581448346e5e98481b05b270ac722
Zach Calvert <ZachCalvert@gmail.com>
2018-06-20 22:08:48 -0500

    Created starting point for import command

--- ps-import.js
+++ ps-import.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+
+var program = require('commander');
+
+program
+  .command('import <dir> <managed>')
+  .action(function(dir, managed, cmd) {
+    console.log('import directory: %s', dir);
+    console.log('managed directory: %s', managed);
+  })
+
+program.parse(process.argv);
