97ec529924a1e7dd986d6b388b6430e7b3a52437
Zach Calvert <ZachCalvert@gmail.com>
2018-06-24 06:54:33 -0500

    Initialization function in place

--- pic-man/PicManInit.js
+++ pic-man/PicManInit.js
@@ -1,5 +1,7 @@
 
-var fs = require ('fs')
+var fs = require ('fs');
+var writeFile = require('write');
+var mkdirp = require('mkdirp');
 
 module.exports = class PicManInit {
 
@@ -8,10 +10,24 @@ module.exports = class PicManInit {
   }
 
   init() {
-    if (fs.existsSync(this.folder)) {
-      console.log("'%s' exists", this.folder);
+    var folder = this.folder;
+    if (fs.existsSync(folder)) {
+      fs.readdir(folder, function(err, files) {
+        if (err) {
+           console.error("Error occurred during reading of '%s'", folder, err)
+        } else {
+           if (files.length > 0) {
+              console.error("%s is not empty and has not been initialized for media management.", folder)
+           } else {
+            writeFile.sync(folder + "/pic-man.db", "");
+           }
+        }
+      });
     } else {
-      console.log("'%s' does not exist");
+      mkdirp(folder, function (err) {
+        if (err) console.error(err)
+        else writeFile.sync(folder + "/pic-man.db", "");
+      });
     }
   }
 }
