f022d36aa9037926e9fa259e062e413a00d59344
Zach Calvert <ZachCalvert@gmail.com>
2018-06-30 21:19:54 -0500

    Deleting unused file

--- pic-man/FileProcessor.js
+++
@@ -1,8 +0,0 @@
-
-var sha256File = require('sha256-file');
-
-module.exports = function (file, managed) {
-    console.log("Wil run sha of %s", file)
-    var sha256Sum = sha256File(file);
-    console.log("%s sums to %s", file, sha256Sum);
-};
